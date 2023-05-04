956b27dc0fb7e0ce63cf887450c5059aeacf6235
Malcev Nikita <maltsew.na@gmail.com>
2023-05-04 23:27:43 +0300

    В проект добавлена реализация OpenAPI с помощью 'drf-spectacular' В файле setting добавлена рекомендуемая конфигурация spectacular. В url прописаны маршруты получения схемы api с помощью swagger и redoc

--- friends/settings.py
+++ friends/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'drf_yasg',
+    'drf_spectacular',
+    'rest_framework',
+    'friendship',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +130,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Your Project API',
+    'DESCRIPTION': 'Your project description',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    # OTHER SETTINGS
+}
