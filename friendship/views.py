3b0ee5e43135c774f19f416989d203ef257f5626
Malcev Nikita <maltsew.na@gmail.com>
2023-05-09 20:23:41 +0300

    Изменение регистрации нового пользователя в соответствие новой модели User Добавлен контроллер для отправки заявки в друзья другому пользователю: from_user определяется из request to_user - как маршрут при отправке запроса /friend-requests/

--- friendship/views.py
+++ friendship/views.py
@@ -1,14 +1,40 @@
 from django.shortcuts import render
 from rest_framework.permissions import AllowAny
-from rest_framework import viewsets
+from django.core.exceptions import ObjectDoesNotExist
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from . import serializers
+from django.http import Http404
 
 # Create your views here.
-from .models import CustomUser
-from .serializers import RegisterSerializer
+from .models import User, FriendshipRequest
+from .serializers import RegisterSerializer, FriendshipRequestSerializer
 from rest_framework import generics
+from rest_framework.views import APIView
 
 
 class RegisterView(generics.CreateAPIView):
-    queryset = CustomUser.objects.all()
+    queryset = User.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer
+
+
+class CreateFriendRequestView(generics.ListCreateAPIView):
+    queryset = FriendshipRequest.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = FriendshipRequestSerializer
+
+    def post(self, request, *args, **kwargs):
+        try:
+            to_user = User.objects.get(pk=kwargs['pk'])
+        except ObjectDoesNotExist:
+            raise Http404("Does not exist")
+        friend_obj = FriendshipRequest.objects.create(
+            from_user=request.user,
+            to_user=to_user,
+        request_status=2)
+        return Response(
+            FriendshipRequestSerializer(friend_obj).data,
+            status.HTTP_201_CREATED
+        )
