75fb8f3fa598b3a5b85067534bb91923ca842fa9
Malcev Nikita <maltsew.na@gmail.com>
2023-05-09 20:20:29 +0300

    Добавлены маршруты для аутенификации с помощью djoser Добавлен маршрут отправки заявки в друзья friend-request/{user_id}, по которому пользователь из request отправляет заявку

--- friendship/urls.py
+++ friendship/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from friendship.views import RegisterView
+from rest_framework import routers
+from .views import CreateFriendRequestView
 
+router = routers.DefaultRouter()
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
+    path('friend-requests/<int:pk>', CreateFriendRequestView.as_view(), name='friend-request'),
+    path('auth/', include('djoser.urls')),
+    re_path(r'^auth/', include('djoser.urls.authtoken')),
 ]
