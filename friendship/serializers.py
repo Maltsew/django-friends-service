2466ee992b0344d7f8931cd2be76df4f7f0d1919
Malcev Nikita <maltsew.na@gmail.com>
2023-05-09 20:27:32 +0300

    Добавлены сериализаторы для модели User, определяющий метод создания нового пользователя через эндпоинт register/, а также для модели FriendshipRequest, определяющий отображение статуса заявок на дружбу

---
+++ friendship/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from .models import User, FriendshipRequest
+from django.contrib.auth import authenticate
+
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(required=True)
+
+    class Meta:
+        model = User
+        fields = ('username',)
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+        )
+        user.save()
+        return user
+
+
+class FriendshipRequestSerializer(serializers.ModelSerializer):
+
+
+    class Meta:
+        model = FriendshipRequest
+        fields = ('id', 'from_user', 'to_user', 'request_status')
