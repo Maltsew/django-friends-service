ea691952098485a6059d53166e0cb67ab8d552d2
Malcev Nikita <maltsew.na@gmail.com>
2023-05-09 12:44:33 +0300

    Добавлены модели: CustomUser для описания пользователя FriendshipRequest для описания отношений заявок в друзья Friends для описания дружеский отношений В админке зарегистрированы данные модели

--- friendship/admin.py
+++ friendship/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import CustomUser, FriendshipRequest, Friends
+from django.contrib import auth
 
-# Register your models here.
+
+admin.site.unregister(auth.models.Group)
+
+
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username',)
+    search_fields = ('title',)
+
+
+class FriendshipRequestAdmin(admin.ModelAdmin):
+    list_display = ('from_user', 'to_user',)
+
+
+class FriendsAdmin(admin.ModelAdmin):
+    list_display = ('from_user', 'to_user',)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(FriendshipRequest, FriendshipRequestAdmin)
+admin.site.register(Friends, FriendsAdmin)
--- friendship/models.py
+++ friendship/models.py
@@ -1,3 +1,68 @@
 from django.db import models
 
 # Create your models here.
+class CustomUser(models.Model):
+    """
+    Модель пользователя для сервиса друзей
+    """
+    username = models.CharField(max_length=150, unique=True, help_text='Обязательное поле', verbose_name='username')
+
+    def __str__(self):
+        return f"username: {self.username}"
+
+
+class FriendshipRequest(models.Model):
+    """
+    Модель отображения состояния заявок дружбы
+    """
+    REQUEST_STATUS = (
+        (1, 'None'),
+        (2, 'Pending'),
+        (3, 'Accept'),
+        (4, 'Reject'),
+    )
+    from_user = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='friendship_request_sender'
+    )
+    to_user = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='friendship_request_receiver'
+    )
+    request_status = models.PositiveSmallIntegerField(
+      choices=REQUEST_STATUS,
+      default=1,
+   )
+
+    class Meta:
+        verbose_name = 'Заявка в друзья'
+        verbose_name_plural = 'Заявки в друзья'
+        # нельзя создать заявку на дружбу самому себе
+        unique_together = ('from_user', 'to_user')
+
+    def __str__(self):
+        return f"username {self.from_user} created friendship request to {self.to_user}"
+
+class Friends(models.Model):
+    from_user = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='user'
+    )
+    to_user = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='friend'
+    )
+
+    class Meta:
+        verbose_name = 'Друг'
+        verbose_name_plural = 'Друзья'
+        # нельзя быть другом самому себе
+        unique_together = ('from_user', 'to_user')
+
+
+    def __str__(self):
+        return f"username {self.from_user} друг для {self.to_user}"
